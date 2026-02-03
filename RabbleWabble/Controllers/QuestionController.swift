5fd86fce2be0141d4fb5f490de91a3eebff29372
Edwin Cardenas <ecardenas@lwolf.com>
2026-02-03 17:04:06 -0500

    refactor: set up mvc folder structure

--- RabbleWabble/Controllers/QuestionController.swift
+++ RabbleWabble/Controllers/QuestionController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  QuestionController.swift
 //  RabbleWabble
 //
 //  Created by Edwin Cardenas on 2/3/26.
@@ -7,9 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+public class QuestionController: UIViewController {
 
-    override func viewDidLoad() {
+    public override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemPink
--- RabbleWabble/SceneDelegate.swift
+++ RabbleWabble/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         window = UIWindow(windowScene: scene)
 
-        window?.rootViewController = ViewController()
+        window?.rootViewController = QuestionController()
         window?.makeKeyAndVisible()
     }
 
