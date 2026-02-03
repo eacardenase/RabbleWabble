af284aa50879d47066a2ab2f468c3b3e74b43fde
Edwin Cardenas <ecardenas@lwolf.com>
2026-02-03 17:16:06 -0500

    feat: add mock questions and assets

---
+++ RabbleWabble/Models/Question.swift
@@ -0,0 +1,16 @@
+//
+//  Question.swift
+//  RabbleWabble
+//
+//  Created by Edwin Cardenas on 2/3/26.
+//
+
+import Foundation
+
+public struct Question {
+
+    public let answer: String
+    public let hint: String?
+    public let prompt: String
+
+}
---
+++ RabbleWabble/Models/QuestionGroup.swift
@@ -0,0 +1,16 @@
+//
+//  QuestionGroup.swift
+//  RabbleWabble
+//
+//  Created by Edwin Cardenas on 2/3/26.
+//
+
+import Foundation
+
+public struct QuestionGroup {
+
+    public let questions: [Question]
+    public let title: String
+
+}
+
